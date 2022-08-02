76b1a68142ad32601e1e030c5a305562354055b5
Muhammad Azhar <muhammad1azhar2@gmail.com>
2022-08-02 15:38:11 +0500

    rem warnings

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
-import {RegisterPage, SignInPage, UploadVideoPage} from './Pages/index';
-import {authUserStateObserver,signOutTenant} from './Services/firebase';
+import {authUserStateObserver} from './Services/firebase';
 import Navigation from './Config/router';
 function App() {
   const [currentUser, setCurrentUser] = useState('');
@@ -13,10 +12,6 @@ function App() {
     
     })
   },[])
-
-
-
- 
     return (
     
       <Navigation user={currentUser}/>
